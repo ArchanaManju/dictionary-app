8110d419c737e86844bdb574d06ac9eb56c1a764
ArchanaManju <archanahm2021@gmail.com>
2022-04-07 22:52:41 +0100

    added axio

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
+
+  function handelResponse(response) {
+    console.log(response.data);
+  }
   function search(event) {
     event.preventDefault();
     alert(`searching ${keyWord}`);
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    axios.get(apiUrl).then(handelResponse);
   }
   function handleKeywordChange(event) {
     setKeyWord(event.target.value);
