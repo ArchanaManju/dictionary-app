9e2e001a8244d1495fc6c3d7bc76149dcc29a6c6
ArchanaManju <archanahm2021@gmail.com>
2022-04-12 21:39:56 +0100

    adding results to UI

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
+  let [results, setResults] = useState(null);
 
   function handelResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
   function search(event) {
     event.preventDefault();
-    alert(`searching ${keyWord}`);
+
     //api Documentation https://dictionaryapi.dev/
     const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
     axios.get(apiUrl).then(handelResponse);
@@ -24,6 +26,7 @@ export default function Dictionary() {
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
         <button>Search</button>
       </form>
+      <Results result={results} />
     </div>
   );
 }
