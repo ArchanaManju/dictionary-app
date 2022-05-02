4c9311e00afb7adbf8c7d70aa4cce5192fe054ed
ArchanaManju <archanahm2021@gmail.com>
2022-05-02 22:07:00 +0100

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -14,7 +14,6 @@ export default function Meaning(props) {
             <strong>Example:</strong>
             <em>{definition.example}</em>
             <br />
-
             <Synonyms synonyms={definition.synonyms} />
           </div>
         );
